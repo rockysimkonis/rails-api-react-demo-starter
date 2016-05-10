98d8768dc3ab1e60c83251f33118d73ade698256
Rocky Simkonis <rokas.simkonis@gmail.com>
2016-05-09 21:01:38 -0500

    Authentication with Twitter OAuth and JWT

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   match '*all', to: 'application#preflight', via: [:options]
+
+  get 'current_user', to: 'application#current_user'
+  get 'request_token', to: 'tokens#request_token'
+  get 'access_token', to: 'tokens#access_token'
+  get 'client_token', to: 'tokens#client_token'
+
   resources :blabs, only: [:index]
 end
